8951adeae798924906182c0dfcbedc114f79ff4a
Tania Shevchuk <96tania96@gmail.com>
2025-06-21 16:56:06 +0300

    Add custom fixtures for LoginPage and MainPage

--- Hooks/testHooks.ts
+++ Hooks/testHooks.ts
@@ -1,6 +1,22 @@
 import { test as base, expect } from '@playwright/test';
+import { LoginPage } from '../PageObject/loginPage_PO';
+import { MainPage } from '../PageObject/mainPage_PO';
 
-const test = base; 
+type Fixtures = {
+  loginPage: LoginPage;
+  mainPage: MainPage;
+};
+
+const test = base.extend<Fixtures>({
+  loginPage: async ({ page }, use) => {
+    const loginPage = new LoginPage(page);
+    await use(loginPage); 
+  },
+  mainPage: async ({ page }, use) => {
+    const mainPage = new MainPage(page);
+    await use(mainPage);
+  }
+});
 
 test.afterEach(async ({ page }, testInfo) => {
 	const safeTitle = testInfo.title.replace(/[^a-zA-Z0-9-_]/g, '_');
--- Tests/loginPage.spec.ts
+++ Tests/loginPage.spec.ts
@@ -1,18 +1,10 @@
 import {validData, unregisteredData} from "../TestData/loginData.ts";
-import { LoginPage} from '../PageObject/loginPage_PO'
-import { MainPage} from '../PageObject/mainPage_PO'
 import { test, expect } from '../Hooks/testHooks';
 import { allure } from 'allure-playwright';
 
-test('Login with valid user', async ({ page }) => {
-	const loginPage = new LoginPage(page);
-    const mainPage = new MainPage(page)
+test('Login with valid user', async ({ loginPage, mainPage, page }) => {
 
-	try{
-	await allure.step('Open login page', async () => {
-		await loginPage.openLoginPage();
-	});
-
+try{
 	await allure.step('Login with valid credentials', async () => {
 		await loginPage.loginWithEmail(validData.email, validData.password);
 	});
@@ -26,31 +18,21 @@ test('Login with valid user', async ({ page }) => {
         expect(userName).toEqual(validData.profileName)
 	});
 } finally{
-
     await allure.attachment('Screenshot', await page.screenshot(), 'image/png');
 }
 });
 
 
-test("Login with unregistered data on Engenious University", async ({page})=> {
+test("Login with unregistered data on Engenious University", async ({loginPage, page})=> {
 
-    const loginPage = new LoginPage(page)
-    
-	try{
-    await allure.step('Open login page', async () => {
-		await loginPage.openLoginPage();
-	});
-
-    await allure.step('Login with unregistered credentials', async () => {
+try{
+	await allure.step('Login with unregistered credentials', async () => {
 		await loginPage.loginWithEmail(unregisteredData.email,unregisteredData.password);
 	});
 
-    const errorMessage  = await loginPage.getErrorMessage()
-    expect(errorMessage).toEqual("Invalid credentials!")
-
     await allure.step('Verify error message', async () => {
 		 const errorMessage  = await loginPage.getErrorMessage()
-    expect(errorMessage).toEqual("Invalid credentials!")
+    	expect(errorMessage).toEqual("Invalid credentials!")
 	});
 	} finally{
 		await allure.attachment('Screenshot', await page.screenshot(), 'image/png');
