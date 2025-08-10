520c2ce85592d8719807734701a393637adc718d
Tania Shevchuk <96tania96@gmail.com>
2025-08-10 20:41:22 +0300

    feat: add Allure tags utility

--- Utility/testUtility.ts
+++ Utility/testUtility.ts
@@ -1,8 +1,12 @@
-import { allure } from "allure-playwright";
-import { Page } from "@playwright/test";
+import { allure } from 'allure-playwright';
+import { Page } from '@playwright/test';
 
-export async function attachScreenshot(page: Page, title = "Screenshot") {
-  await allure.step("Attach screenshot on failure", async () => {
-    await allure.attachment(title, await page.screenshot(), "image/png");
+export async function attachScreenshot(page: Page, title = 'Screenshot') {
+  await allure.step('Attach screenshot on failure', async () => {
+    await allure.attachment(title, await page.screenshot(), 'image/png');
   });
+}
+
+export function setAllureTags(tags: string[]): void {
+  tags.forEach((tag) => allure.label('tag', tag));
 }
