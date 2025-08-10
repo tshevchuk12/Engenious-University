04f15a33bf30a7a492816ada7753328f07af2673
Tania Shevchuk <96tania96@gmail.com>
2025-08-10 20:35:56 +0300

    add Prettier config

---
+++ prettier.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  semi: true,
+  tabWidth: 2,
+  printWidth: 400,
+  trailingComma: 'all',
+  arrowParens: 'always',
+  endOfLine: 'lf',
+};
