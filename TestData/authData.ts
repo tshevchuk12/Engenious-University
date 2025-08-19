b75516b4d4f229bc838a8a3dd7c464f121460b2b
Tania Shevchuk <96tania96@gmail.com>
2025-08-19 16:14:07 +0300

    ci: disable tests temporarily for GitHub Actions run

---
+++ TestData/authData.ts
@@ -0,0 +1,27 @@
+import { faker } from "@faker-js/faker";
+
+export const validData = {
+  email: "your_email@example.com",
+  password: "your_password",
+  profileName: "Your Name",
+};
+
+export const unregisteredData = {
+  email: faker.internet.email(),
+  password: "Qwert12!Q",
+};
+
+export const invalidData = {
+  email: faker.internet.email().replace(".com", ""),
+  empty: "",
+};
+
+export const loginUIErrorMessages = {
+  invalidCredentials: "Invalid credentials!",
+  emailErrorMessage: "Invalid email address",
+  passwordErrorMessage: "Password is required",
+};
+
+export const loginAPIErrorMessages = {
+  invalidCredentials: { message: "Invalid credentials!", error: "Unauthorized", statusCode: 401 },
+};
