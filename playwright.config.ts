3302cdfe7a7400306fd51f731c8ee2f84c4c25ad
Tania Shevchuk <96tania96@gmail.com>
2025-08-19 15:52:37 +0300

    ci: add GitHub Actions config for test execution

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,9 +12,10 @@ import { defineConfig, devices } from "@playwright/test";
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: "./tests",
+  testDir: "./Tests",
   /* Run tests in files in parallel */
   fullyParallel: true,
+  testMatch: ['**/*.spec.ts', '**/*.test.ts'],
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
